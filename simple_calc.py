6b39a68e9abb98a4f978493941eef847ad73520d
Rahul Anand <rahul1989@gmail.com>
2015-06-01 20:44:10 +0530

    just a simple calculator

---
+++ simple_calc.py
@@ -0,0 +1,23 @@
+def add(a,b):
+	print a+b
+
+def sub(a,b):
+	print a-b
+
+def mul(a,b):
+	print a*b
+
+def div(a,b):
+	print a/b
+
+def pow(a,b):
+	print a**b
+
+x=int(raw_input("Enter the first number:"))
+y=int(raw_input("Enter the second number:"))
+
+add(x, y)
+sub(x,y)
+mul(x,y)
+div(x,y)
+pow(x,y)
