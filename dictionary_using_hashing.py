4ab33c9ed6e35e88779bac54dfb63107a8ca4820
Rahul Anand <rahul1989@gmail.com>
2015-06-18 22:00:05 +0530

    Added comments

--- dictionary_using_hashing.py
+++ dictionary_using_hashing.py
@@ -1,5 +1,5 @@
 class IntegerDict(object):
-
+#an implementation of an integer dictionary where it is assumed that all the keys are integers.
 	def __init__(self,num_buckets):
 		#Create an empty dictionary
 		self.buckets = []
@@ -16,6 +16,7 @@ class IntegerDict(object):
 		hash_bucket.append((dic_key,dic_val))
 
 	def get_value(self, dic_key):
+		#return the value associated with the given key
 		hash_bucket = self.buckets[dic_key%self.num_buckets]
 		for n in hash_bucket:
 			if n[0] == dic_key:
