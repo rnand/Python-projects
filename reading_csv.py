ec56217db437940b2abac7a5bc0bc73efb7fef1c
Rahul Anand <rahul1989@gmail.com>
2015-06-20 22:28:46 +0530

    Fixed

--- reading_csv.py
+++ reading_csv.py
@@ -1,7 +1,10 @@
 import csv
+import os
 
+INPUT_DIR=raw_input("Enter the location of csv:")
+filename=raw_input("Enter the filename:")
 for filename in os.listdir(INPUT_DIR):
-   with open(os.path.join(INPUT_DIR,filename), dialect='excel-tab') as infile:
+   with open(os.path.join(INPUT_DIR,filename)) as infile:
       reader = csv.reader(infile)
       for row in reader:
           print row
