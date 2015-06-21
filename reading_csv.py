93aa93a66b8b38f48310af8afbd3b5432766887f
Rahul Anand <rahul1989@gmail.com>
2015-06-21 21:50:50 +0530

    Small bug fix

--- reading_csv.py
+++ reading_csv.py
@@ -4,7 +4,7 @@ import os
 INPUT_DIR=raw_input("Enter the location of csv:")
 filename=raw_input("Enter the filename:")
 for filename in os.listdir(INPUT_DIR):
-   with open(os.path.join(INPUT_DIR,filename)) as infile:
-      reader = csv.reader(infile)
+   with open(os.path.join(INPUT_DIR,filename),'rb') as infile:
+      reader = csv.reader(infile, dialect='excel-tab')
       for row in reader:
           print row
