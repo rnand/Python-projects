19ad66c07e94b4d79249812ea27d6be2c2d47bb5
Rahul Anand <rahul1989@gmail.com>
2015-06-22 22:46:45 +0530

    Comments for readability

--- reading_csv.py
+++ reading_csv.py
@@ -4,7 +4,7 @@ import os
 INPUT_DIR=raw_input("Enter the location of csv:")
 filename=raw_input("Enter the filename:")
 for filename in os.listdir(INPUT_DIR):
-   with open(os.path.join(INPUT_DIR,filename),'rb') as infile:
+   with open(os.path.join(INPUT_DIR,filename),'rb') as infile:  #generate proper path to the file and open it
       reader = csv.reader(infile, dialect='excel-tab')
       for row in reader:
           print row
